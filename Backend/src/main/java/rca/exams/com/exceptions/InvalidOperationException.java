452530d208121a2be29ed1f311c5e95a1709b2be
gajurosine <gajurosine1@gmail.com>
2023-12-18 12:11:30 +0200

    working on InvalidOperationException

--- Backend/src/main/java/rca/exams/com/exceptions/InvalidOperationException.java
+++ Backend/src/main/java/rca/exams/com/exceptions/InvalidOperationException.java
@@ -2,7 +2,8 @@ package Backend.src.main.java.rca.exams.com.exceptions;
 
 
 public class InvalidOperationException extends Exception {
-    public InvalidOperationException(String message) {
+    public InvalidOperationException(String message)
+    {
         super(message);
     }
 }
